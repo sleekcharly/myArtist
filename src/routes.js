d2b8d79944d56fc7ac3a9018096857bcb9dece1f
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-15 05:30:23 +0100

    fourth commit

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Routes = () => {
+import Header from './components/Header';
+
+const Routes = () => (
     <BrowserRouter>
-        
+        <Header />
+
     </BrowserRouter>
 
-};
+);
 
 export default Routes;
