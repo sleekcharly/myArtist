92526cc1ed8bcc6d47485a3a98a0bb0037c9d2df
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-15 05:22:16 +0100

    added components directory

---
+++ src/route.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    <BrowserRouter>
+        
+    </BrowserRouter>
+
+};
+
+export default Routes;
